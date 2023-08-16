1b49b29d278418e0eb9e59594f1c1121d58f381b
Giovana Gomes <118355750+gomes21gi@users.noreply.github.com>
2023-08-16 18:38:09 -0300

    16/08

---
+++ bombeiroSa-main/bombeiroSa-main/bombeiroSa/bombeirosa.sql
@@ -0,0 +1,66 @@
+-- -- phpMyAdmin SQL Dump
+-- -- version 5.1.1
+-- -- https://www.phpmyadmin.net/
+-- --
+-- -- Host: 127.0.0.1
+-- -- Tempo de geração: 13-Jul-2023 às 15:31
+-- -- Versão do servidor: 8.0.21
+-- -- versão do PHP: 8.1.2
+
+-- SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+-- START TRANSACTION;
+-- SET time_zone = "+00:00";
+
+
+-- /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+-- /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+-- /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+-- /*!40101 SET NAMES utf8mb4 */;
+
+-- --
+-- -- Banco de dados: `bombeirosa`
+-- --
+
+-- -- --------------------------------------------------------
+
+-- --
+-- -- Estrutura da tabela `paciente`
+-- --
+
+-- CREATE TABLE `paciente` (
+--   `id_paciente` int NOT NULL,
+--   `nome_paciente` int NOT NULL,
+--   `idade_paciente` int NOT NULL,
+--   `rg_cpf_paciente` int NOT NULL,
+--   `sexo_paciente` int NOT NULL,
+--   `fone_paciente` int NOT NULL,
+--   `acompanhante_paciente` int NOT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- -- --------------------------------------------------------
+
+-- --
+-- -- Estrutura da tabela `pre-hospitalar`
+-- --
+
+-- CREATE TABLE `pre-hospitalar` (
+--   `id_pre_hospitalar` int NOT NULL,
+--   `ocorrencia_hospitalar` int NOT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- -- --------------------------------------------------------
+
+-- --
+-- -- Estrutura da tabela `usuario`
+-- --
+
+-- CREATE TABLE `usuario` (
+--   `usuario_id` int NOT NULL,
+--   `nome_usuario` int NOT NULL,
+--   `senha_usuario` int NOT NULL
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+-- COMMIT;
+
+-- /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+-- /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+-- /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
