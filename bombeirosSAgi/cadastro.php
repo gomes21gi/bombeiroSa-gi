5de2c3400ea67fceda36e2781f878e75b154b046
Giovana Gomes <118355750+gomes21gi@users.noreply.github.com>
2023-10-25 18:28:37 -0300

    Add files via upload

--- bombeirosSAgi/cadastro.php
+++ bombeirosSAgi/cadastro.php
@@ -5,7 +5,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $sql = "INSERT INTO usuarios (email, senha) VALUES (?, ?)";
+    $sql = "INSERT INTO id_usuarios (email, senha) VALUES (?, ?)";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param("ss", $email, $senha);
 
